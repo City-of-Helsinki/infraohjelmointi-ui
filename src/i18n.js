f0a977641756424b3161ce1ec7a8278eaa8c672e
Jonas Engberg <jonas.engberg@outlook.com>
2022-11-15 17:16:40 +0200

    removed detecting of user-language since MVP version wont be using the swedish localization

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationFI from './i18n/fi.json';
 import translationSE from './i18n/se.json';
@@ -19,8 +18,6 @@ const resources = {
 i18n
   // load translation using http
   .use(Backend)
-  // detect user language
-  .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
