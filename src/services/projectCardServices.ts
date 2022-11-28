8c01c85dfa68e91f58762d4ca8ffb4c7571ac322
Kreshnik Gunga <kreshnik.gunga@robu.fi>
2022-11-28 09:34:16 +0200

    Make API URL constant

--- src/services/projectCardServices.ts
+++ src/services/projectCardServices.ts
@@ -2,30 +2,32 @@ import { IError } from '@/interfaces/common';
 import { IProjectCard } from '@/interfaces/projectCardInterfaces';
 import axios from 'axios';
 
+const { REACT_APP_API_URL } = process.env;
+
 export const getProjectCards = async (): Promise<Array<IProjectCard>> => {
   return axios
-    .get(`${process.env.REACT_APP_API_URL}/projects/`)
+    .get(`${REACT_APP_API_URL}/projects/`)
     .then((res) => res.data)
     .catch((err: IError) => Promise.reject(err));
 };
 
 export const getProjectCard = async (id: string): Promise<IProjectCard> => {
   return axios
-    .get(`${process.env.REACT_APP_API_URL}/projects/${id}`)
+    .get(`${REACT_APP_API_URL}/projects/${id}`)
     .then((res) => res.data)
     .catch((err: IError) => Promise.reject(err));
 };
 
 export const postProjectCard = async (projectCard: IProjectCard): Promise<void> => {
   return axios
-    .post(`${process.env.REACT_APP_API_URL}/projects/`, projectCard)
+    .post(`${REACT_APP_API_URL}/projects/`, projectCard)
     .then((res) => res.data)
     .catch((err: IError) => Promise.reject(err));
 };
 
 export const deleteProjectCard = async (id: string): Promise<void> => {
   return axios
-    .delete(`${process.env.REACT_APP_API_URL}/projects/${id}`)
+    .delete(`${REACT_APP_API_URL}/projects/${id}`)
     .then((res) => res.data)
     .catch((err: IError) => Promise.reject(err));
 };
