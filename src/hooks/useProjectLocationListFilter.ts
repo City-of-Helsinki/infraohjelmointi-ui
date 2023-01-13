7c8bbcdb8e08a4bede287a0209a52bb58a2ccbaf
Jonas Engberg <jonas.engberg@outlook.com>
2023-01-13 09:25:18 +0200

    fixed PR issues

--- src/hooks/useProjectLocationListFilter.ts
+++ src/hooks/useProjectLocationListFilter.ts
@@ -9,42 +9,52 @@ const useProjectLocationListFilter = () => {
   const districts = useAppSelector((state: RootState) => state.location.districts, _.isEqual);
   const divisions = useAppSelector((state: RootState) => state.location.divisions, _.isEqual);
   const subDivisions = useAppSelector((state: RootState) => state.location.subDivisions, _.isEqual);
-  const districtsList = useAppSelector((state: RootState) => state.lists.district);
+  const districtsList = useAppSelector((state: RootState) => state.lists.district, _.isEqual);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const selectedDistrict = districts.find((d) => d.id === project?.projectLocation);
+    const selectedDistrict = project
+      ? districts.find((d) => d.id === project.projectLocation)
+      : undefined;
 
     if (districtsList.length <= 0) {
       dispatch(setDistrictList(districts));
     }
 
-    if (project?.projectLocation) {
+    const projectLocation = project?.projectLocation;
+
+    if (projectLocation) {
       /* If selected is a district, we need to find all divisions that have that district as their parent */
       if (selectedDistrict) {
-        dispatch(setDivisionList(divisions.filter((d) => d.parent === project?.projectLocation)));
+        dispatch(setDivisionList(divisions.filter((d) => d.parent === projectLocation)));
       }
 
-      const selectedDivision = divisions.find((d) => d.id === project?.projectLocation);
+      const selectedDivision = divisions.find((d) => d.id === projectLocation);
       /* If selected is a division, we need to find the district and find all divisions for that district */
       if (selectedDivision) {
-        const districtForDivision = districts.find((d) => d.id === selectedDivision?.parent);
+        const districtForDivision = districts.find((d) => d.id === selectedDivision.parent);
 
         dispatch(setDivisionList(divisions.filter((d) => d.parent === districtForDivision?.id)));
-        dispatch(
-          setSubDivisionList(subDivisions.filter((sd) => sd.parent === project?.projectLocation)),
-        );
+        dispatch(setSubDivisionList(subDivisions.filter((sd) => sd.parent === projectLocation)));
       }
 
-      const selectedSubDivision = subDivisions.find((sd) => sd.id === project?.projectLocation);
+      const selectedSubDivision = subDivisions.find((sd) => sd.id === projectLocation);
       /* If selected is a subDivision, we need to find the division and district and find which divisions belong to that 
         district and which subDivisions belong to that division */
       if (selectedSubDivision) {
-        const divForSubDiv = divisions.find((c) => c.id === selectedSubDivision?.parent);
+        const divForSubDiv = divisions.find((c) => c.id === selectedSubDivision.parent);
         const districtForDivision = districts.find((d) => d.id === divForSubDiv?.parent);
 
-        dispatch(setSubDivisionList(subDivisions.filter((sd) => sd.parent === divForSubDiv?.id)));
-        dispatch(setDivisionList(divisions.filter((d) => d.parent === districtForDivision?.id)));
+        dispatch(
+          setSubDivisionList(
+            divForSubDiv ? subDivisions.filter((sd) => sd.parent === divForSubDiv.id) : [],
+          ),
+        );
+        dispatch(
+          setDivisionList(
+            districtForDivision ? divisions.filter((d) => d.parent === districtForDivision.id) : [],
+          ),
+        );
       }
     }
 
