e772f9dd4b19e468d8fc21466b100d114ad86608
Jonas Engberg <jonas.engberg@outlook.com>
2023-01-19 14:46:13 +0200

    show add to project button for a newly created hashtag

--- src/components/Project/ProjectHashTags/NewHashTagsForm.tsx
+++ src/components/Project/ProjectHashTags/NewHashTagsForm.tsx
@@ -1,8 +1,8 @@
-import { Paragraph, TextField } from '@/components/shared';
+import { Paragraph, Span, TextField } from '@/components/shared';
 import useHashTagsForm from '@/hooks/useHashTagsForm';
 import { IHashTagsForm } from '@/interfaces/formInterfaces';
 import { Button } from 'hds-react/components/Button';
-import { IconPlus } from 'hds-react/icons';
+import { IconCheck, IconPlus } from 'hds-react/icons';
 import { memo, useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -12,14 +12,18 @@ const NewHashTagsForm = () => {
   const { formMethods, formValues } = useHashTagsForm();
   const { control, reset, handleSubmit } = formMethods;
   const [createNewMode, setCreateNewMode] = useState(false);
+  const [responseHashTag, setResponseHashTag] = useState('');
   const { t } = useTranslation();
 
   const handleCreateNewMode = useCallback(() => setCreateNewMode((current) => !current), []);
 
   const submitNewHashTag = async (form: IHashTagsForm) => {
     console.log('Submit form: ', form);
+    setResponseHashTag(form.hashTag);
     reset(formValues);
   };
+
+  const addToProject = () => console.log('Hashtag should be added to project...');
 
   return isAdmin ? (
     <div className="dialog-section">
@@ -28,12 +32,26 @@ const NewHashTagsForm = () => {
         <Button variant="secondary" iconLeft={<IconPlus />} onClick={handleCreateNewMode}>
           {t('createNewHashTag')}
         </Button>
-        {createNewMode && (
-          <div className="new-hashtags-input">
-            <TextField control={control} name="hashTag" label={t('createNewHashTag')} />
-            <Button onClick={handleSubmit(submitNewHashTag)}>{t('createHashTag')}</Button>
-          </div>
-        )}
+        {createNewMode &&
+          // If a hashtag is successfully created and edit mode it true,
+          // allow admin to add it straight to the project
+          (responseHashTag ? (
+            <div className="new-hashtags-input hashtag-created">
+              <div className="hashtag-created-text">
+                <IconCheck color={'var(--color-success)'} />
+                <Span text={t('hashTagCreated', { responseHashTag })} />
+              </div>
+              <div className="hashtag-created-text">
+                <button onClick={addToProject}>{t('addToProject')}</button>
+              </div>
+            </div>
+          ) : (
+            // If a hashtag isn't created and edit mode is true
+            <div className="new-hashtags-input">
+              <TextField control={control} name="hashTag" label={t('createNewHashTag')} />
+              <Button onClick={handleSubmit(submitNewHashTag)}>{t('createHashTag')}</Button>
+            </div>
+          ))}
       </div>
     </div>
   ) : null;
