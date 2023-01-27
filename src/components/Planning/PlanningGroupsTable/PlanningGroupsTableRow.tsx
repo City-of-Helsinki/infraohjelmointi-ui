3bee60c933c53a4e2473a936749ff2747be04561
Jonas Engberg <jonas.engberg@outlook.com>
2023-01-27 15:22:39 +0200

    status dialog for project table row

--- src/components/Planning/PlanningGroupsTable/PlanningGroupsTableRow.tsx
+++ src/components/Planning/PlanningGroupsTable/PlanningGroupsTableRow.tsx
@@ -1,12 +1,17 @@
+import { useAppDispatch, useAppSelector } from '@/hooks/common';
 import { IProject } from '@/interfaces/projectInterfaces';
 import { planProjectValues } from '@/mocks/common';
+import { getProjectPhasesThunk } from '@/reducers/listsSlice';
+import { RootState } from '@/store';
 import {
+  IconCheck,
+  IconCross,
   IconDocument,
   IconMenuDots,
   IconPlaybackRecord,
   IconSpeechbubbleText,
 } from 'hds-react/icons';
-import { FC } from 'react';
+import { FC, useCallback, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import PlanningGroupsTableCell from './PlanningGroupsTableCell';
 
@@ -38,7 +43,17 @@ const CircleIcon = ({ value }: { value: string }) => (
  */
 const PlanningGroupsTableRow: FC<IPlanningProjectsTableProps> = ({ project }) => {
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   const navigateToProject = () => navigate(`/project/${project.id}/basics`);
+  const [showStatusDialog, setShowStatusDialog] = useState(false);
+  const phases = useAppSelector((state: RootState) => state.lists.phase);
+
+  const toggleStatusDialog = useCallback(() => setShowStatusDialog((current) => !current), []);
+
+  useEffect(() => {
+    dispatch(getProjectPhasesThunk());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <tr>
@@ -47,7 +62,49 @@ const PlanningGroupsTableRow: FC<IPlanningProjectsTableProps> = ({ project }) =>
         <div className="project-header-cell-container">
           {/* LEFT */}
           <div className="left">
-            <IconMenuDots size="xs" />
+            <IconMenuDots size="xs" style={{ cursor: 'pointer' }} onClick={toggleStatusDialog} />
+            {showStatusDialog && (
+              <div
+                style={{
+                  position: 'absolute',
+                  width: '15rem',
+                  background: 'var(--color-white)',
+                  border: '0.1rem solid var(--color-black)',
+                  zIndex: '999',
+                }}
+              >
+                <div
+                  style={{
+                    width: '100%',
+                    borderBottom: '0.08rem solid var(--color-black-20)',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}
+                >
+                  <div>
+                    <p style={{ fontSize: '12px' }}>Hakaniemenpuiston esirak...</p>
+                    <p style={{ fontWeight: 'bold' }}>Nykystatus</p>
+                  </div>
+                  <IconCross />
+                </div>
+
+                {phases.map((p) => (
+                  <div
+                    key={p.id}
+                    style={{
+                      width: '100%',
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <p>{p.value}</p>
+                    <IconCheck />
+                  </div>
+                ))}
+              </div>
+            )}
             <IconDocument size="xs" />
             <button className="project-name-button" onClick={navigateToProject}>
               {project.name}
