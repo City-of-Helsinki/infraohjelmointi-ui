a2ec3a6978fe9ddd49d381f72c0a46a9ed05a11a
Jonas Engberg <jonas.engberg@outlook.com>
2023-02-16 15:30:04 +0200

    removed search filter component since it just opens up the search form

--- src/components/SearchResult/SearchResultHeader.tsx
+++ src/components/SearchResult/SearchResultHeader.tsx
@@ -1,9 +1,15 @@
 import { Title } from '@/components/shared';
 import SearchTerms from './SearchTerms';
-import SearchFilter from './SearchFilter';
 import './styles.css';
+import { Button } from 'hds-react/components/Button';
+import { IconSliders } from 'hds-react/icons';
+import { useAppDispatch } from '@/hooks/common';
+import { useCallback } from 'react';
+import { toggleSearch } from '@/reducers/searchSlice';
 
 const SearchResultHeader = () => {
+  const dispatch = useAppDispatch();
+  const handleOpenSearch = useCallback(() => dispatch(toggleSearch()), [dispatch]);
   return (
     <div className="search-result-header-container">
       <div className="search-result-page-title">
@@ -11,7 +17,16 @@ const SearchResultHeader = () => {
       </div>
       <div className="search-result-terms-and-filters-container">
         <SearchTerms />
-        <SearchFilter />
+        <div className="search-filter-container">
+          <Button
+            variant="secondary"
+            size="small"
+            iconLeft={<IconSliders />}
+            onClick={handleOpenSearch}
+          >
+            Rajaa hakua
+          </Button>
+        </div>
       </div>
     </div>
   );
