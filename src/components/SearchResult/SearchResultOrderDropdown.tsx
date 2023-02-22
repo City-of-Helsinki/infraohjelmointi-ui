913a1d3ed42e63822999e80497e91d36d9c7a5de
Jonas Engberg <jonas.engberg@outlook.com>
2023-02-22 12:37:05 +0200

    changed list order dropdown to feature project ordering instead of hashtag ordering

--- src/components/SearchResult/SearchResultOrderDropdown.tsx
+++ src/components/SearchResult/SearchResultOrderDropdown.tsx
@@ -8,7 +8,7 @@ const SearchResultOrderDropdown = () => {
   const order: Array<IOption> = [
     { value: '1', label: t('searchOrder.new') },
     { value: '2', label: t('searchOrder.old') },
-    { value: '3', label: t('searchOrder.hashTag') },
+    { value: '3', label: t('searchOrder.project') },
     { value: '4', label: t('searchOrder.group') },
   ];
 
