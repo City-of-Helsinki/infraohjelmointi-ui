ac09bf8dddaa407256e2a29698a8f6c6eee60396
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2023-02-23 16:41:22 +0200

    Added validation for location fields

--- src/forms/useGroupForm.ts
+++ src/forms/useGroupForm.ts
@@ -1,10 +1,9 @@
 import { FormField, HookFormControlType, IForm, IGroupForm } from '@/interfaces/formInterfaces';
 
-import { useForm } from 'react-hook-form';
+import { useForm, UseFormGetValues } from 'react-hook-form';
 
 import { t, TFunction } from 'i18next';
 import { useMemo } from 'react';
-import { If2 } from 'reselect/es/types';
 import { IOption } from '@/interfaces/common';
 interface IGroupFormFieldsSplit {
   basic: Array<IForm>;
@@ -20,19 +19,33 @@ const buildGroupFormFields = (
       {
         name: 'name',
         type: FormField.Text,
-        rules: { required: 'This field is required' },
+        rules: { required: t(`fieldRequiredWarning`) },
       },
       {
         name: 'masterClass',
         type: FormField.Select,
         placeholder: 'Valitse',
-        rules: { required: 'This field is required', validate: {isPopulated: (mc: IOption)=> Object.keys(mc).includes('value') && mc.value!=='' ? true : 'This field is required'}},
+        rules: {
+          required: t(`fieldRequiredWarning`),
+          validate: {
+            isPopulated: (mc: IOption) =>
+              Object.keys(mc).includes('value') && mc.value !== ''
+                ? true
+                : t(`fieldRequiredWarning`),
+          },
+        },
       },
       {
         name: 'class',
         type: FormField.Select,
         placeholder: 'Valitse',
-        rules: { required: 'This field is required' , validate: {isPopulated: (c: IOption)=> Object.keys(c).includes('value') && c.value!=='' ? true : 'This field is required'}},
+        rules: {
+          required: t(`fieldRequiredWarning`),
+          validate: {
+            isPopulated: (c: IOption) =>
+              Object.keys(c).includes('value') && c.value !== '' ? true : t(`fieldRequiredWarning`),
+          },
+        },
       },
       {
         name: 'subClass',
@@ -45,11 +58,24 @@ const buildGroupFormFields = (
         name: 'district',
         type: FormField.Select,
         placeholder: 'Valitse',
+
+        rules: {
+          validate: {
+            isPopulated: (d: IOption) =>
+              Object.keys(d).includes('value') && d.value !== '' ? true : t(`fieldRequiredWarning`),
+          },
+        },
       },
       {
         name: 'division',
         type: FormField.Select,
         placeholder: 'Valitse',
+        rules: {
+          validate: {
+            isPopulated: (d: IOption) =>
+              Object.keys(d).includes('value') && d.value !== '' ? true : t(`fieldRequiredWarning`),
+          },
+        },
       },
       {
         name: 'subDivision',
