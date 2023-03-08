7bb8e170d8ac82bea25956896cc092356995f2f7
Jonas Engberg <jonas.engberg@outlook.com>
2023-03-08 16:20:51 +0200

    added category to project header cell and removed unnecessary icons

--- src/components/shared/CustomTag.tsx
+++ src/components/shared/CustomTag.tsx
@@ -5,13 +5,14 @@ interface ICustomTagProps {
   icon?: ReactNode;
   color?: string;
   size?: string;
+  weight?: string;
 }
 
 /**
  * A rounded border tag like a HDS tag, but it fits an icon better and is easily customizeable
  * by us.
  */
-const CustomTag: FC<ICustomTagProps> = ({ icon, text, color, size }) => {
+const CustomTag: FC<ICustomTagProps> = ({ icon, text, color, size, weight }) => {
   return (
     <div
       className="custom-tag-container"
@@ -20,7 +21,12 @@ const CustomTag: FC<ICustomTagProps> = ({ icon, text, color, size }) => {
       }}
     >
       {icon}
-      <span style={{ fontSize: size === 'm' ? '1rem' : '0.875rem' }}>{text}</span>
+      <span
+        className={`font-${weight ? weight : 'bold'}`}
+        style={{ fontSize: size === 'm' ? '1rem' : '0.875rem' }}
+      >
+        {text}
+      </span>
     </div>
   );
 };
