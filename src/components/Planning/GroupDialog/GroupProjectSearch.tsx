f38a186964417d6f0e53034cee54e62ca2425483
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-05-26 15:22:20 +0300

    Fixed linting errors

--- src/components/Planning/GroupDialog/GroupProjectSearch.tsx
+++ src/components/Planning/GroupDialog/GroupProjectSearch.tsx
@@ -3,7 +3,7 @@ import { getProjectsWithParams } from '@/services/projectServices';
 import { arrayHasValue, listItemToOption } from '@/utils/common';
 import { Tag } from 'hds-react/components/Tag';
 import { SearchInput } from 'hds-react/components/SearchInput';
-import { FC, memo, useCallback, useEffect, useRef, useState } from 'react';
+import { FC, memo, useCallback, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Control, Controller, UseFormGetValues } from 'react-hook-form';
 import { IGroupForm } from '@/interfaces/formInterfaces';
