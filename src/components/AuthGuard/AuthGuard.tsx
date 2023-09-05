29553181e75bc71b15405a442da82d429ba47c21
Jonas Engberg <jonas.engberg@outlook.com>
2023-09-05 14:52:21 +0300

    get user from API with profile sid

--- src/components/AuthGuard/AuthGuard.tsx
+++ src/components/AuthGuard/AuthGuard.tsx
@@ -1,12 +1,15 @@
 import { FC, useEffect, useState } from 'react';
 import { useAuth, hasAuthParams } from 'react-oidc-context';
 import { getApiToken } from '@/services/personServices';
+import { useAppDispatch } from '@/hooks/common';
+import { getUserThunk } from '@/reducers/authSlice';
 
 /**
  * Component to handle authentication stuff
  */
 const AuthGuard: FC = () => {
   const auth = useAuth();
+  const dispatch = useAppDispatch();
   const [hasTriedSignin, setHasTriedSignin] = useState(false);
   const { isAuthenticated, activeNavigator, isLoading, user } = auth;
 
@@ -15,6 +18,10 @@ const AuthGuard: FC = () => {
     if (user?.access_token) {
       console.log('auto renew');
       getApiToken();
+      // Get user from our API if there is an sid
+      if (user?.profile?.sid) {
+        dispatch(getUserThunk(user?.profile?.sid as string));
+      }
     }
   }, [user]);
 
