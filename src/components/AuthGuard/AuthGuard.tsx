c724d72818ee244095f8efd1b87de719912a5601
Jonas Engberg <jonas.engberg@outlook.com>
2023-09-12 17:03:11 +0300

    redirect user back to the initially opened page when navigating

--- src/components/AuthGuard/AuthGuard.tsx
+++ src/components/AuthGuard/AuthGuard.tsx
@@ -3,6 +3,9 @@ import { useAuth, hasAuthParams } from 'react-oidc-context';
 import { getApiToken } from '@/services/userServices';
 import { useAppDispatch, useAppSelector } from '@/hooks/common';
 import { getUserThunk, selectUser } from '@/reducers/authSlice';
+import { useLocation, useNavigate } from 'react-router';
+
+const INITIAL_PATH = 'initialPath';
 
 /**
  * Component to handle authentication stuff
@@ -12,6 +15,8 @@ const AuthGuard: FC = () => {
   const dispatch = useAppDispatch();
   const [hasTriedSignin, setHasTriedSignin] = useState(false);
   const user = useAppSelector(selectUser);
+  const location = useLocation();
+  const navigate = useNavigate();
   const { isAuthenticated, activeNavigator, isLoading, user: oidcUser } = auth;
 
   const token = sessionStorage.getItem('infraohjelmointi_api_token');
@@ -22,7 +27,15 @@ const AuthGuard: FC = () => {
       getApiToken().then(() => {
         // Get user from our API if the user doesn't exist oidcUser's id doesn't match the current user
         if (!user || (user?.uuid !== oidcUser.profile.sub && !auth.isLoading)) {
-          dispatch(getUserThunk());
+          dispatch(getUserThunk()).then(() => {
+            // Since the redirect from login will bring the url back to REACT_APP_REDIRECT_URI path, we need to
+            // return the user to the path that it initially navigated to when opening the app
+            const initialPath = localStorage.getItem(INITIAL_PATH);
+            if (initialPath) {
+              navigate(initialPath);
+              localStorage.removeItem(INITIAL_PATH);
+            }
+          });
         }
       });
     }
@@ -31,6 +44,11 @@ const AuthGuard: FC = () => {
   // Check if user exists and sign in
   useEffect(() => {
     if (!hasAuthParams() && !isAuthenticated && !activeNavigator && !isLoading && !hasTriedSignin) {
+      const { search, pathname } = location;
+      const initialPath = search ? `${pathname}${search}` : pathname;
+
+      localStorage.setItem(INITIAL_PATH, initialPath);
+
       auth.signinRedirect();
       setHasTriedSignin(true);
     }
