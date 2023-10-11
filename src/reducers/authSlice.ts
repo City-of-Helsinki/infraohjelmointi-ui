62c8f6221f500636f7720b7db0c7ce779e80417e
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2023-10-11 14:58:25 +0300

    Add hardcoded user roles
    Add ids
    
    Add ids

--- src/reducers/authSlice.ts
+++ src/reducers/authSlice.ts
@@ -22,6 +22,123 @@ const initialState: IUserState = {
   user: null,
   error: null,
 };
+/*
+const coordinator = {
+  "id": "86b826df-589c-40f9-898f-1584e80b5482",
+  "name": "sg_kymp_sso_io_koordinaattorit" as UserRole,
+  "display_name": "sg_KYMP_sso_IO_Koordinaattorit",
+}
+const projectManager = {
+  "id": '31f86f09-b674-4c1d-81db-6d5fe2e587f9',
+  "name": 'sg_kymp_sso_io_projektipaallikot' as UserRole,
+  "display_name": 'sg_KYMP_sso_IO_Projektipaallikot',
+}*/
+const admin = {
+  "id": 'test-admin-role',
+  "name": 'sg_kymp_sso_io_admin' as UserRole,
+  "display_name": 'sg_KYMP_sso_IO_Admin',
+}
+const programmers = {
+  "id": 'da48bfe9-6a99-481f-a252-077d31473c4c',
+  "name": 'sg_kymp_sso_io_ohjelmoijat' as UserRole,
+  "display_name": 'sg_KYMP_sso_IO_Ohjelmoijat',
+}
+/*const projectAreaProgrammers = {
+  "id": '4d229780-b511-4652-b32b-362ad88a7b55',
+  "name": 'sg_kymp_sso_io_projektialueiden_ohjelmoijat' as UserRole,
+  "display_name": 'sg_KYMP_sso_IO_Projektialueiden_ohjelmoijat',
+}
+const viewers = {
+  "id": '7e39a13e-bd48-43ab-bd23-738e73b5137a',
+  "name": 'sl_dyn_kymp_sso_io_katselijat' as UserRole,
+  "display_name": 'sl_dyn_kymp_sso_io_katselijat',
+} */
+
+const users = [
+  {
+    "id": "e0c8c3bd-ac21-48fe-849a-d23415a27c03",
+    "role": "admin"
+  },
+  {
+    "id": "b1700ee9-4e9a-4d27-b964-389bfc076d57",
+    "role": "admin"
+  },
+  {
+    "id": "b36bcad4-e705-4de7-baf2-ff8af0188af0",
+    "role": "admin"
+  },
+  {
+    "id": "28c46fc9-15bd-4727-8db1-9c8da67b97cb",
+    "role": "admin"
+  },
+  {
+    "id": "93476b18-3320-44e1-9a49-b8b7bf59a114",
+    "role": "admin"
+  },
+  {
+    "id": "d21f89b4-2c17-4ad0-956c-3e1e3cfb204f",
+    "role": "admin"
+  },
+  {
+    "id": "ca7a50c1-4d6a-4c74-bf6b-38584317e2a8",
+    "role": "programmer"
+  },
+  {
+    "id": "3eed85b4-7285-4d5f-a86a-94b0e777d9af",
+    "role": "programmer"
+  },
+  {
+    "id": "06d1ed99-617b-4ffc-9d91-25707a69f5f8",
+    "role": "programmer"
+  },
+  {
+    "id": "126ec360-1722-492b-9506-ef58548f1d43",
+    "role": "programmer"
+  },
+  {
+    "id": "210a5af6-957f-431f-b484-8e25e42140a8",
+    "role": "programmer"
+  },
+  {
+    "id": "6a6fd6ef-dcc6-42f2-93ac-26de4a49b3da",
+    "role": "programmer"
+  },
+  {
+    "id": "77484fbc-fc25-472c-adee-0e661141abd1",
+    "role": "programmer"
+  },
+  {
+    "id": "d1d8595c-2edf-42df-b1bf-43e758393e83",
+    "role": "programmer"
+  }
+]
+
+const setHardCodedUserRoles = (payload: IUser) => {
+  // Check if user is found from the list and then assign the correct role
+  const user = users.find(user => user.id === payload.uuid);
+  
+  switch (user?.role) {
+    case "admin":
+      return {
+        ...payload,
+         ad_groups: [
+          admin,
+         ],
+      };
+    case "programmer":
+      return {
+        ...payload,
+         ad_groups: [
+          programmers,
+         ],
+      };
+    default:
+      return {
+        ...payload,
+          ad_groups: [],
+      };
+  }   
+}
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -29,48 +146,7 @@ export const authSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getUserThunk.fulfilled, (state, action: PayloadAction<IUser>) => {
-      const userWithRoles = {
-        ...action.payload,
-        // ad_groups: [
-        //   {
-        //     id: '86b826df-589c-40f9-898f-1584e80b5482',
-        //     name: 'sg_kymp_sso_io_koordinaattorit' as UserRole,
-        //     display_name: 'sg_KYMP_sso_IO_Koordinaattorit',
-        //   },
-        //   {
-        //     id: '31f86f09-b674-4c1d-81db-6d5fe2e587f9',
-        //     name: 'sg_kymp_sso_io_projektipaallikot' as UserRole,
-        //     display_name: 'sg_KYMP_sso_IO_Projektipaallikot',
-        //   },
-        //   {
-        //     id: 'test-admin-role',
-        //     name: 'sg_kymp_sso_io_admin' as UserRole,
-        //     display_name: 'sg_KYMP_sso_IO_Admin',
-        //   },
-        //   {
-        //     id: 'da48bfe9-6a99-481f-a252-077d31473c4c',
-        //     name: 'sg_kymp_sso_io_ohjelmoijat' as UserRole,
-        //     display_name: 'sg_KYMP_sso_IO_Ohjelmoijat',
-        //   },
-        //   {
-        //     id: '4d229780-b511-4652-b32b-362ad88a7b55',
-        //     name: 'sg_kymp_sso_io_projektialueiden_ohjelmoijat' as UserRole,
-        //     display_name: 'sg_KYMP_sso_IO_Projektialueiden_ohjelmoijat',
-        //   },
-        //   {
-        //     id: '31f86f09-b674-4c1d-81db-6d5fe2e587f9',
-        //     name: 'sg_kymp_sso_io_projektipaallikot' as UserRole,
-        //     display_name: 'sg_KYMP_sso_IO_Projektipaallikot',
-        //   },
-        //   {
-        //     id: '7e39a13e-bd48-43ab-bd23-738e73b5137a',
-        //     name: 'sl_dyn_kymp_sso_io_katselijat' as UserRole,
-        //     display_name: 'sl_dyn_kymp_sso_io_katselijat',
-        //   },
-        // ],
-      };
-
-      return { ...state, user: userWithRoles };
+      return { ...state, user: setHardCodedUserRoles(action.payload) };
     });
     builder.addCase(getUserThunk.rejected, (state, action: PayloadAction<IError | unknown>) => {
       return { ...state, error: action.payload };
