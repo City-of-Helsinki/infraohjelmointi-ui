72e4eab34aec8aaf75212964e3b57ef90085457c
Saija Seppä <78361679+saijaseppa@users.noreply.github.com>
2024-02-02 14:16:35 +0200

    Update SaveIndicator.tsx

--- src/components/Project/ProjectBasics/ProjectFormSidePanel/SaveIndicator.tsx
+++ src/components/Project/ProjectBasics/ProjectFormSidePanel/SaveIndicator.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { CustomTag } from '@/components/shared';
 import { useAppSelector } from '@/hooks/common';
-import { selectIsProjectSaving, selectProject } from '@/reducers/projectSlice';
+import { selectIsProjectSaving, selectProject, selectProjectMode } from '@/reducers/projectSlice';
 import moment from 'moment';
 import './styles.css';
 
@@ -10,8 +10,22 @@ const SaveIndicator = () => {
   const project = useAppSelector(selectProject);
   const isSaving = useAppSelector(selectIsProjectSaving);
   const updatedMoment = moment(project?.updatedDate).format('D.M.YYYY HH:mm:ss');
+  const projectMode = useAppSelector(selectProjectMode);
 
-  return (
+  if (projectMode === 'new') {
+    return(
+    <div className="mt-4 flex justify-start">
+      <CustomTag
+        color={'var(--color-success)'}
+        text={isSaving ? 'Tallennetaan' : 'Ei tallennettu'}
+        weight={'light'}
+        textColor={'var(--color-white)'}
+        showLoading={isSaving}
+      />
+    </div>)
+  }
+  else {
+    return (
     <div className="mt-4 flex justify-start">
       <CustomTag
         color={'var(--color-success)'}
@@ -20,8 +34,8 @@ const SaveIndicator = () => {
         textColor={'var(--color-white)'}
         showLoading={isSaving}
       />
-    </div>
-  );
+    </div>)
+  }
 };
 
 export default SaveIndicator;
