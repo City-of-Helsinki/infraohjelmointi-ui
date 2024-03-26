0306eb1b39ff477b03c4248e929fc0f6d2b680f2
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2024-03-26 12:30:32 +0200

    fix: hide also the extra rows

--- src/utils/reportHelpers.ts
+++ src/utils/reportHelpers.ts
@@ -456,13 +456,14 @@ export const convertToReportRows = (coordinatorRows: IPlanningRow[], reportType:
         // TA parts that don't have any planned budgets shouldn't be shown on the report
         if (plannedBudgets.some((value) => value !== "0")) {
           forcedToFrameHierarchy.push(convertedClass);
-        }
-        // If the class is on the fourth level, we want to add some extra rows there
-        if (/^\d \d\d \d\d \d\d/.test(c.name)) {
-          const extraRows = getExtraRows(c, categories);
-          extraRows.forEach((row) =>
-            forcedToFrameHierarchy.push(row)
-          );
+
+          // If the class is on the fourth level, we want to add some extra rows there
+          if (/^\d \d\d \d\d \d\d/.test(c.name)) {
+            const extraRows = getExtraRows(c, categories);
+            extraRows.forEach((row) =>
+              forcedToFrameHierarchy.push(row)
+            );
+          }
         }
       }
       return forcedToFrameHierarchy;
