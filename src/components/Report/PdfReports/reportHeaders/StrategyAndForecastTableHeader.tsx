b9567ebb1698291ff314a48d585b4ae8fb5c85a1
Markus Palomäki <markus.palomaki@futurice.com>
2025-04-24 17:12:44 +0300

    feat: IO-449 add kilo euros on header row and styling

--- src/components/Report/PdfReports/reportHeaders/StrategyAndForecastTableHeader.tsx
+++ src/components/Report/PdfReports/reportHeaders/StrategyAndForecastTableHeader.tsx
@@ -40,6 +40,7 @@ const styles = StyleSheet.create({
   },
   budgetCell: {
     ...cellStyles,
+    paddingRight: '3px',
     width: '80px',
   },
   monthCell: {
@@ -67,13 +68,13 @@ const StrategyAndForecastTableHeader = ({ isForecastReport }: StrategyTableHeade
         <Text style={styles.projectCell}>{`\n${t('report.strategy.projectNameTitle')}`}</Text>
         <Text style={styles.projectManagerCell}>{`${t('report.strategy.projectsTitle')}\n${t('report.strategy.projectManagerTitle')}`}</Text>
         <Text style={styles.projectPhaseCell}>{`\n${t('projectPhase')}`}</Text>
-        <Text style={styles.budgetCell}>{`\nTA ${year}`}</Text>
-        <Text style={styles.budgetCell}>{`\nTS ${year}`}</Text>
+        <Text style={styles.budgetCell}>{`${t('report.shared.ta')} ${year}\n${t('report.shared.kiloEuro')}`}</Text>
+        <Text style={styles.budgetCell}>{`${t('report.shared.ts')} ${year}\n${t('report.shared.kiloEuro')}`}</Text>
         {
           isForecastReport ?
             <>
-              <Text style={styles.budgetCell}>{`\nEnnuste ${year}`}</Text>
-              <Text style={styles.budgetCell}>{`\nPoikkeama`}</Text>
+              <Text style={styles.budgetCell}>{`${t('report.forecastReport.forecastValue')} ${year}\n${t('report.shared.kiloEuro')}`}</Text>
+              <Text style={styles.budgetCell}>{`${t('report.forecastReport.differenceValue')}\n${t('report.shared.kiloEuro')}`}</Text>
             </> : null
         }
         <Text style={styles.monthCell}>{`${year}\n01`}</Text>
