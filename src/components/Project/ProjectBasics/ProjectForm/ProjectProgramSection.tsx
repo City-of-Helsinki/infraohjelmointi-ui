d89aec4c89bb8ab4c1b7ec730e9652d9d7a99d38
Milla Eerola <milla.eerola@futurice.com>
2025-04-25 10:34:40 +0300

    feat: IO-426 add validation for when other reason is selected

--- src/components/Project/ProjectBasics/ProjectForm/ProjectProgramSection.tsx
+++ src/components/Project/ProjectBasics/ProjectForm/ProjectProgramSection.tsx
@@ -1,13 +1,15 @@
 import { FormSectionTitle, SelectField } from '@/components/shared';
 import TextAreaField from '@/components/shared/TextAreaField';
-import { FC, memo } from 'react';
-import { Control } from 'react-hook-form';
+import { FC, memo, useMemo } from 'react';
+import { Control, UseFormGetValues } from 'react-hook-form';
 import { IProjectForm } from '@/interfaces/formInterfaces';
 import { useOptions } from '@/hooks/useOptions';
 import { validateMaxLength } from '@/utils/validation';
 import { t } from 'i18next';
+import { IOption } from '@/interfaces/common';
 
 interface IProjectProgramSectionProps {
+  getValues: UseFormGetValues<IProjectForm>;
   getFieldProps: (name: string) => {
     name: string;
     label: string;
@@ -16,8 +18,42 @@ interface IProjectProgramSectionProps {
   isUserOnlyViewer: boolean;
   isInputDisabled: boolean;
 }
-const ProjectProgramSection: FC<IProjectProgramSectionProps> = ({ getFieldProps, isUserOnlyViewer, isInputDisabled }) => {
+const ProjectProgramSection: FC<IProjectProgramSectionProps> = ({ getValues, getFieldProps, isUserOnlyViewer, isInputDisabled }) => {
   const budgetOverrunReasons = useOptions('budgetOverrunReasons');
+
+  const validateBudgetOverrunReason = useMemo(
+    () => ({
+      validate: {
+        isBudgetOverrunReasonValid: (budgetOverrunReason: IOption) => {
+          const mappedBudgetOverrunReason = budgetOverrunReasons.find(item => item.value === budgetOverrunReason.value);
+          const otherReason = getValues('otherBudgetOverrunReason');
+
+          if (mappedBudgetOverrunReason?.label == 'otherReason' && otherReason == '') {
+            return t('validation.required', { field: t('projectForm.otherBudgetOverrunReason') });
+          }
+          return true;
+        },
+      },
+    }),
+    [budgetOverrunReasons, getValues],
+  );
+
+  const validateOtherReasonField = useMemo(
+    () => ({
+      validate: {
+        isOtherReasonFieldValid: (otherReasonField: string) => {
+          const budgetOverrunReason = budgetOverrunReasons.find(item => item.value === getValues('budgetOverrunReason').value);
+          const isBudgetOverrunReasonOtherReason = budgetOverrunReason?.label == 'otherReason'
+
+          if (isBudgetOverrunReasonOtherReason && otherReasonField == '') {
+            return t('validation.required', { field: t('projectForm.otherBudgetOverrunReason') });
+          }
+          return true;
+        },
+      },
+    }),
+    [budgetOverrunReasons, getValues],
+  );
 
   return (
     <div className="w-full" id="basics-location-section">
@@ -27,6 +63,7 @@ const ProjectProgramSection: FC<IProjectProgramSectionProps> = ({ getFieldProps,
           <SelectField
               {...getFieldProps('budgetOverrunReason')}
               options={budgetOverrunReasons}
+              rules={validateBudgetOverrunReason}
               size='full'
               disabled={isInputDisabled}
               readOnly={isUserOnlyViewer}
@@ -37,7 +74,7 @@ const ProjectProgramSection: FC<IProjectProgramSectionProps> = ({ getFieldProps,
         <div className="form-col-xxl">
           <TextAreaField
             {...getFieldProps('otherBudgetOverrunReason')}
-            rules={{ ...validateMaxLength(200, t) }}
+            rules={{ ...validateMaxLength(200, t), ...validateOtherReasonField }}
             readOnly={isUserOnlyViewer}
           />
         </div>
