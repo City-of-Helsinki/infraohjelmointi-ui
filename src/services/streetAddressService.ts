e36c18692adec4fb02a3368ab5194dbd7563bd40
Sari <122358667+sarivah@users.noreply.github.com>
2026-04-27 09:48:59 +0300

    IO-844 Fixed postal code search: if no street number is provided, leave it out of the search as it is not required and automatically adding 1 for street number caused no results when that number was not found

--- src/services/streetAddressService.ts
+++ src/services/streetAddressService.ts
@@ -16,14 +16,15 @@ function getStreetName(input: string) {
  * Extracts the trailing numeric sequence from a street address string.
  *
  * @param input - The street address that may end with a numeric component.
- * @returns The numeric substring found at the end of the address, or `'1'` if no digits are present.
+ * @returns The complete search parameter for street number for the numeric
+ * substring found at the end of the address, or '' if no digits are present.
  */
 function getStreetNumber(input: string) {
   const matches = /\d{1,5}$/.exec(input);
   if (matches) {
-    return matches[0];
+    return `AND(osoitenumero=%27${matches[0]}%27)`;
   }
-  return '1';
+  return '';
 }
 
 /**
@@ -39,7 +40,7 @@ export async function fetchAddressData(address: string) {
   let url =
     'https://kartta.hel.fi/ws/geoserver/avoindata/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAME=avoindata:Helsinki_osoiteluettelo&OUTPUTFORMAT=json&SORTBY=katunimi,osoitenumero&COUNT=300';
 
-  url += `&CQL_FILTER=((katunimi%20ILIKE%20%27${streetName}%25%27%20OR%20gatan%20ILIKE%20%27${streetName}%25%27)AND(osoitenumero=%27${streetNumber}%27))`;
+  url += `&CQL_FILTER=((katunimi%20ILIKE%20%27${streetName}%25%27%20OR%20gatan%20ILIKE%20%27${streetName}%25%27)${streetNumber})`;
 
   const response = await fetch(url);
   return response.json();
