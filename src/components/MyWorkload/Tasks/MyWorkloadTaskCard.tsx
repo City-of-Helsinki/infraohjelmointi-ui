9d4f09062db8114804a6c5436b6d9b9f19a340d3
Marko Haarni <marko.haarni@gofore.com>
2026-09-11 13:40:37 +0300

    fix: fix Sonar issues

--- src/components/MyWorkload/Tasks/MyWorkloadTaskCard.tsx
+++ src/components/MyWorkload/Tasks/MyWorkloadTaskCard.tsx
@@ -48,9 +48,7 @@ const MyWorkloadTaskCard: FC<MyWorkloadTaskCardProps> = ({ task }) => {
         <div className={classes.taskColumns}>
           <p>
             <IconMoneyBag aria-hidden="true" />
-            {`${t(`myWorkloadView.tasks.budget.${task.taskType}`)}: ${formatBudgetEuro(
-              task.budget,
-            )}`}
+            {t(`myWorkloadView.tasks.budget.${task.taskType}`)}: {formatBudgetEuro(task.budget)}
           </p>
           <p>
             <IconHammers aria-hidden="true" />
